086a2fb988ad54133e9ef070f25770d65499cdca
Zoe Aspenns <aspennza@mail.uc.edu>
2024-09-24 11:05:15 -0400

    Scanner and variables added

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,19 @@
+import java.util.Scanner;
+
 public class Main
 {
     public static void main(String[] args)
     {
-        
+        //num LEGAL_AGE = 21
+        //num userAge = 0
+        Scanner in = new Scanner(System.in);
+        final int LEGAL_AGE = 21;
+        int userAge = 0;
+
+        //output “Enter your age: ”
+        //input userAge
+        //if userAge >= LEGAL_AGE then
+        //output “Congratulations! You get a wristband.”
+        //endIf
     }
 }
