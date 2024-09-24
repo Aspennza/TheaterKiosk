8d08a45ea2fd018ae321eed0c957b8eefc79d221
Zoe Aspenns <aspennza@mail.uc.edu>
2024-09-24 11:13:58 -0400

    Complete program draft--comments still included

--- src/Main.java
+++ src/Main.java
@@ -9,11 +9,34 @@ public class Main
         Scanner in = new Scanner(System.in);
         final int LEGAL_AGE = 21;
         int userAge = 0;
+        String trash = "";
 
         //output “Enter your age: ”
+
+        System.out.println("Enter your age: ");
+
         //input userAge
+
+        if (in.hasNextInt())
+        {
+            userAge = in.nextInt();
+            in.nextLine();
+        }
+        else
+        {
+            trash = in.nextLine();
+            System.out.println("\nPlease enter an integer value. You entered: " + trash + ".");
+            System.out.println("\nPlease run the program again.");
+            System.exit(0);
+        }
+
         //if userAge >= LEGAL_AGE then
         //output “Congratulations! You get a wristband.”
         //endIf
+
+        if (userAge >= LEGAL_AGE)
+        {
+            System.out.println("Congratulations! You get a wristband.");
+        }
     }
 }
